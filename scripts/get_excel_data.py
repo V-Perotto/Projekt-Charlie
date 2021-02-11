c8aa11673488fc881f20b1f354c1053bcc948527
V-Perotto <vittorio.perotto@outlook.com>
2021-02-11 19:05:57 -0300

    Alterando funções

--- scripts/get_excel_data.py
+++ scripts/get_excel_data.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 def get_user_data(path, user):
     way = path + '/' + user
-    data = pd.read_excel(way) 
-    login = pd.DataFrame(data, columns= ['username','password'])
-    print (login)
+    data = pd.read_excel(way, usecols= ['username','password'])
+    login = data.to_dict()
+    return login
 
 def get_materia_data(path, materia):
     way = path + '/' + materia
-    data = pd.read_excel(way) 
-    course = pd.DataFrame(data, columns=['Materias', 'Horarios', 'DiaDaSemana', 'Semestre', 'SemestreAtual', 'Status', 'InicioAula', 'FimAula'])
-    print (course)
+    data = pd.read_excel(way, usecols=['Materias', 'Horarios', 'DiaDaSemana', 'Semestre', 'SemestreAtual', 'Status', 'InicioAulas', 'FimAulas'])
+    course  = data.to_dict() 
+    return course
