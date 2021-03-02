6652626df4ac1bcdf2d1be4072edf091ca623a5d
V-Perotto <vittorio.perotto@outlook.com>
2021-03-02 14:26:14 -0300

    Adicionando um arquivo de logs em python

---
+++ filelogger.py
@@ -0,0 +1,35 @@
+
+from os import getcwd 
+from os.path import isfile
+from os.path import join
+from os import listdir
+from datetime import datetime
+
+# Obrigado Fabrício por me mostrar como criar um log em python e por ser um
+# colega incrível :)
+def filelog(name, desc):
+    print("ENTROU")
+    
+    if name == "PYTHON":
+        path_way = getcwd()
+        path_logs = path_way + "/results/log"
+
+    # Obtem a data atual e hora
+    data = datetime.now().strftime("%d-%m-%Y")
+    data_hora = datetime.now().strftime("%d-%m-%Y__%H:%M:%S")
+
+    if (len(listdir(path_logs)) != 0):
+        onlyfiles = [f for f in listdir(path_logs) if isfile(join(path_logs, f))]
+
+        if (True in [True for x in onlyfiles if (data in x)]):
+
+            with open(join(path_logs,"CHARLIE_"+ data + ".txt"), "a") as file:
+                file.write(data_hora+" : " + desc + '\n')
+            return
+
+    with open(join(path_logs,"CHARLIE_"+ data + ".txt"), "a") as file:
+        file.write("===================================================\n" +
+                   "\t\t\t\tPROJEKT CHARLIE\n" +    # pov: cursed python
+                   "===================================================\n\n\n" + 
+                   data_hora + " : " + desc + '\n')
+    return
